90293fce73d7866c526739d58f43aeea1d4a77a7
Nikita Medvedev <ahujadis@mail.ru>
2019-02-28 00:39:53 +0500

    Работает, но немного по другому нежели задумывалось

--- exercises/logpuzzle/logpuzzle.py
+++ exercises/logpuzzle/logpuzzle.py
@@ -89,18 +89,18 @@ def main():
     if not args:
         print('usage: [--todir dir] logfile')
         sys.exit(1)
+    rabotaet()
+    #todir = ''
+    #if args[0] == '--todir':
+      #  todir = args[1]
+     #   del args[0:2]
 
-    todir = ''
-    if args[0] == '--todir':
-        todir = args[1]
-        del args[0:2]
+    #img_urls = read_urls(args[0])
 
-    img_urls = read_urls(args[0])
-
-    if todir:
-        download_images(img_urls, todir)
-    else:
-        print('\n'.join(img_urls))
+    #if todir:
+     #   download_images(img_urls, todir)
+    #else:
+    #    print('\n'.join(img_urls))
 
 if __name__ == '__main__':
     main()
